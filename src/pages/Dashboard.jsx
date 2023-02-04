85121fbbea6597c0816ffa7d490dda944b7089a3
Max <57855468+musaCODEzz@users.noreply.github.com>
2023-02-04 08:29:15 +0300

    Dashboard content

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "../styles/dashboard.css";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="dashboard">
+      <div className="dashboard__wrapper">
+        <div className="dashboard__cards">
+          <div className="single__card">
+            <div className="card__content">
+              <h4>Total Cars</h4>
+              <span>50+</span>
+            </div>
+            <span><i class="ri-police-car-line"></i></span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
