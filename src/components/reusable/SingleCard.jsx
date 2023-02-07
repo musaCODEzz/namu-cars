298b796253f4f205bb5b004813e6f507a79d8ef4
Max <57855468+musaCODEzz@users.noreply.github.com>
2023-02-07 09:04:11 +0300

    Dashboard content

---
+++ src/components/reusable/SingleCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const SingleCard = () => {
+  return (
+    <div className="single__card">
+            <div className="card__content">
+              <h4>Total Cars</h4>
+              <span>50+</span>
+            </div>
+            <span className='card__icon'>
+              <i class="ri-police-car-line"></i>
+            </span>
+    </div>
+  )
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,13 +6,15 @@ const Dashboard = () => {
     <div className="dashboard">
       <div className="dashboard__wrapper">
         <div className="dashboard__cards">
-          <div className="single__card">
+        <div className="single__card">
             <div className="card__content">
               <h4>Total Cars</h4>
               <span>50+</span>
             </div>
-            <span><i class="ri-police-car-line"></i></span>
-          </div>
+            <span className='card__icon'>
+              <i class="ri-police-car-line"></i>
+            </span>
+    </div>
         </div>
       </div>
     </div>
